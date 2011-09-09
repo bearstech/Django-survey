9d38760e35ceb467c4025309a861fe96205266d4
Jérôme Blondon <jblondon@bearstech.com>
2011-09-09 11:38:43 +0200

    added support for django csrf

--- survey/static/survey/js/survey/survey.js
+++ survey/static/survey/js/survey/survey.js
@@ -4,6 +4,16 @@ Ext.onReady(function() {
 	Ext.QuickTips.init();
 	Ext.ux.msgBus = new Ext.ux.MessageBus();
 
+	Ext.Ajax.on('beforerequest', function (conn, options) {
+		   if (!(/^http:.*/.test(options.url) || /^https:.*/.test(options.url))) {
+		     if (typeof(options.headers) == "undefined") {
+		       options.headers = {'X-CSRFToken': Ext.util.Cookies.get('csrftoken')};
+		     } else {
+		       options.headers.extend({'X-CSRFToken': Ext.util.Cookies.get('csrftoken')});
+		     }                        
+		   }
+		}, this);
+	
 	var viewport = new Ext.Viewport({
 		layout : 'fit',
 		items : [ {
